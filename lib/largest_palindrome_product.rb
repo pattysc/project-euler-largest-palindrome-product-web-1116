033d6cce9dbd2f829f39832f3ed0df9bbc0c7eae
pattysc <pattys@users.noreply.github.com>
2017-01-19 10:19:26 -0500

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,38 @@
 # Implement your procedural solution here!
+def palindrome?(string)
+	l = string.length - 1
+	first = 0
+	last = l/2
+
+	i = 0
+	until i > last
+		if string[i] == string[l]
+			i+=1
+			l-=1
+		else
+			return false
+		end
+	end
+		return true
+end
+
+def largest_palindrome_product
+	index = 999
+	bigpal = 1
+
+	while index > 99
+		index2 = 999
+		while index2 > 90
+			pal = (index * index2).to_s
+			if palindrome?(pal)
+				if (pal.to_i) > bigpal
+				 	bigpal = pal.to_i
+				 end
+			end
+			index2 -= 1
+		end
+		index -= 1
+	end
+
+	return bigpal
+end
